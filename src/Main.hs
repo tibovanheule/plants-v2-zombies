b6655a6a370ca1d26c541e26f662b99a04fd1049
Tibo Vanheule <tibo@tibovanheule.space>
2020-07-16 14:05:32 +0200

    main

--- src/Main.hs
+++ src/Main.hs
@@ -1,4 +1,52 @@
+-- | Main graphic entry
 module Main where
+import System.Directory
+import Types
+import Game
+import System.Environment
+import System.IO
+import Data.Either
+import System.Exit
+import Control.Applicative
+import Control.Monad
+import System.FilePath
+import Graphical
 
+--Ongeldige bestanden mag je in deze modus gewoon negeren.
 main :: IO ()
-main = return ()
+main = do
+          args <- getArgs
+          let debug = "debug" `elem` args
+          -- Haal programma argumenten op.
+          fp <- head <$> getArgs
+          -- Bestaat de directory.
+          doesDirectoryExist fp >>= \x -> when (not x) (die "directory not found")
+          -- Zoek in de map alle level files.
+          contents <- getDirFilesPaths fp
+          -- DEBUG print
+          when debug (print contents)
+          -- Parse all level files
+          parsedFiles <-  readLevelFiles contents
+          -- DEBUG print
+          when debug (print parsedFiles)
+          -- Are there any successfully parsed files?
+          case parsedFiles of
+                 [] -> die "No readably files"
+                 _ ->  when debug (print "Levels loaded")
+          let game = createPossibleGame (rights parsedFiles)
+          graphic game
+
+-- | Get all FilePaths of the files in a directory.
+getDirFilesPaths :: FilePath -> IO [FilePath]
+getDirFilesPaths fp = map ((fp ++ "/") ++) . filter check  <$> getDirectoryContents fp
+                      where check x = isfile x && islevelfile x
+                            isfile x = notElem x [".",".."]
+                            islevelfile x = drop (length x - 9) x  == ".level.in"
+
+-- | Convert array of filepaths to array of parsed levels.
+readLevelFiles :: [FilePath] -> IO [Either Error Level]
+readLevelFiles = mapM readLevelFile
+
+-- | Parse filepath to parsed level.
+readLevelFile :: FilePath -> IO (Either Error Level)
+readLevelFile file = readFile file >>= \x -> return (getLevel x)
--- src/Test.hs
+++ src/Test.hs
@@ -1,4 +1,24 @@
 module Main where
+import System.Environment
+import System.Exit
+import Game
+import Types
+import System.IO
+import Data.Either
 
 main :: IO ()
-main = return ()
+main = do args <- getArgs
+          contentLevel <- readFile (head args)
+          let level = getLevel contentLevel
+          case level of
+               (Left err) -> die ("Error parsing level" ++ err)
+               _ -> print "Successfully parsed level"
+          contentDefence <- readFile (last args)
+          let defence = getDefence contentDefence
+          case defence of
+                (Left _) -> die "Error parsing Defence"
+                _ -> print "Successfully parsed defence"
+          let leveldef = (head $ rights [level]) {plants=head $ rights [defence]}
+          print leveldef
+          let game = createGame leveldef
+          runTest game
